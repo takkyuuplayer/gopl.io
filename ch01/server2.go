239f5f534d09b1fae39c44111d02eb2141cffc57
takkyuuplayer <takkyuuplayer@gmail.com>
2017-10-11 21:55:50 -0400

    Run webserver on docker

--- ch01/server2.go
+++ ch01/server2.go
@@ -13,7 +13,7 @@ var count int
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/count", counter)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
--- ch01/server4.go
+++ ch01/server4.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
