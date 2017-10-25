a1e22ab92e4916d4e554c18e969326a9ad40522b
takkyuuplayer <takkyuuplayer@gmail.com>
2017-10-25 14:31:44 -0400

    Add ex 2.3

---
+++ ex/ch02/popcount/popcount.go
@@ -0,0 +1,32 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(
+		pc[byte(x>>(0*8))] +
+			pc[byte(x>>(1*8))] +
+			pc[byte(x>>(2*8))] +
+			pc[byte(x>>(3*8))] +
+			pc[byte(x>>(4*8))] +
+			pc[byte(x>>(5*8))] +
+			pc[byte(x>>(6*8))] +
+			pc[byte(x>>(7*8))])
+}
+
+// ex 2.3
+func PopCountByLoop(x uint64) int {
+	ret := pc[byte(x>>(0*8))]
+
+	for i := 1; i < 8; i++ {
+		ret += pc[byte(x>>(uint64(i)*8))]
+	}
+
+	return int(ret)
+}
